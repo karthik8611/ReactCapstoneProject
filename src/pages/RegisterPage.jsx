7c268c324ea9ce9e60fdcde3eb2243661ec11344
karthik8611 <mkarthik6768@gmail.com>
2023-10-28 21:48:40 +0530

    added register page

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,14 @@
+import Banner from '../components/Register/Banner'
+import RegisterForm from '../components/Register/RegisterForm'
+
+
+const RegisterPage = ()=>{
+    return(
+        <div style={{display:"flex"}}>
+        <Banner/>
+        <RegisterForm/>
+        </div>
+    )
+}
+
+export default RegisterPage
